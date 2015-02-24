d6c4c26f7e40a995e7e1fa8cfd457ea10c43bb66
Dave Rigby <daver@couchbase.com>
2015-02-24 16:23:32 +0000

    increase_ulimit.sh: Script to bump ulimit on client nodes

---
+++ increase_ulimit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Increase ulimit on client machines
+# TODO: move into client template.
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source ${DIR}/gcloud_inc.sh
+
+CLIENT="cb-client"
+
+for i in $(seq 9 32); do
+    echo "* Increaing 'nofile' ulimit on client $i to 10240"
+    gcloud_ssh ${CLIENT}-${i} "sudo bash -c 'echo -e \"@adm\thard\tnofile\t10240\" >> /etc/security/limits.conf'"
+done
